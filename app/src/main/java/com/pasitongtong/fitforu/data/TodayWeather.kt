f947dce0af5bbbe516f9612cd2dd887ca17bbd66
최연식 <2021110450@dgu.ac.kr>
2025-12-01 01:11:12 +0900

    Delete app directory

--- app/src/main/java/com/pasitongtong/fitforu/data/TodayWeather.kt
+++
@@ -1,9 +0,0 @@
-package com.pasitongtong.fitforu.data
-
-data class TodayWeather(
-    val temp: Double,
-    val maxTemp: Double,
-    val minTemp: Double,
-    val yesterdayAvg: Double,
-    val description: String
-)
